21690ada0ecb2277852f9fd1f0dd1c453e93d830
shamay-lab <shamaylab13@gmail.com>
2025-02-16 20:59:59 +0530

    Update example commands

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Results reported in the paper with default setting:
-CUDA_VISIBLE_DEVICES=0 python predict.py --filename="data/HCMV.txt"
+CUDA_VISIBLE_DEVICES=0 python predict.py --filename="data/SV40.txt"
 
 # Perform prediction with a sliding length of 100: 
-CUDA_VISIBLE_DEVICES=0 python predict.py --slide 100 --filename="data/hg38_Jun.txt" --outfilename="hg38_Jun_predictions_slide_100.csv"
+CUDA_VISIBLE_DEVICES=0 python predict.py --slide 100 --filename="data/hg38_Fos.txt" --outfilename="hg38_Fos_predictions_slide_100.csv"
 
 # Perform post-processing with a threshold percentile of 75: 
 CUDA_VISIBLE_DEVICES=0 python predict.py --threshold 75 --filename="data/hg38_Jun.txt" --outfilename="hg38_Jun_predictions_percentile_75.csv"
